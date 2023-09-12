0e80f67dcce2a0de0daa947afb00abb6d59a8170
Ryan McKinley <mckinley.261@buckeyemail.osu.edu>
2023-09-12 12:18:50 -0400

    Test Walkthrough

--- back-end/delete-this.js
+++ back-end/delete-this.js
@@ -1,2 +1,6 @@
 // Delete or rename this file just doing this for the back-end folder
 import React, { useRef, useEffect, useState } from "react";
+
+export default function Test() {
+    console.log("Hello World");
+}; 
