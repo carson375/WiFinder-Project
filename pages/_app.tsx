5bbb50b6278164b9f66192a0595e4db7c6a2c867
Zach Goodwin <zachgoodwin24@gmail.com>
2023-09-13 13:54:54 -0400

    Added header for amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import "../styles/globals.css";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({...awsExports, ssr: true });
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
 function MyApp({ Component, pageProps }: AppProps) {
