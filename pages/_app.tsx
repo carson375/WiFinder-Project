1a997772912768453d7e1f835d6ac2f770cb3f2f
carson375 <carson.375@osu.edu>
2023-09-14 13:00:27 -0400

    Made the navbar still appear

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import "../styles/globals.css";
 import awsExports from "../src/aws-exports";
-import { Amplify } from 'aws-amplify';
+import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
 
-Amplify.configure({...awsExports, ssr: true });
+Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Authenticator>
-      <Component {...pageProps} />;
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Authenticator>
   );
 }
