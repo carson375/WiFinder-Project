11448261717cac79f136e8889e8446bbdb0c2fb0
Zach Goodwin <zachgoodwin24@gmail.com>
2023-09-15 12:30:28 -0400

    added s3 bucket

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -25,5 +25,11 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     }
+  },
+  "storage": {
+    "s3wifinderprojectstorage0b006b33": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
