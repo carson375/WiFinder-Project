330a022d20c17b163c151888f56e0a85aaf64ae6
Ryan McKinley <mckinley.261@buckeyemail.osu.edu>
2023-10-03 11:34:13 -0400

    Initial changes

--- back-end/delete-this.js
+++ back-end/delete-this.js
@@ -4,3 +4,55 @@ import React, { useRef, useEffect, useState } from "react";
 export default function Test() {
     console.log("Hello World");
 }; 
+
+// Import the Axios library
+const axios = require('axios');
+
+// Define the URL you want to send requests to
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+// Heat Map: Send a GET request 
+axios.get(apiUrl)
+  .then(response => {
+    console.log('GET Request Response:', response.data);
+  })
+  .catch(error => {
+    console.error('GET Request Error:', error);
+  });
+
+// Flight Path Data Page: Send a POST request with data
+const postData = {
+  title: 'foo',
+  body: 'bar',
+  userId: 1,
+};
+axios.post(apiUrl, postData)
+  .then(response => {
+    console.log('POST Request Response:', response.data);
+  })
+  .catch(error => {
+    console.error('POST Request Error:', error);
+  });
+
+  // Flight Path Data Page: Send a GET request 
+axios.get(apiUrl)
+.then(response => {
+  console.log('GET Request Response:', response.data);
+})
+.catch(error => {
+  console.error('GET Request Error:', error);
+});
+
+// Flight Path Page: Send a POST request with data
+const postData2 = {
+  title: 'foo',
+  body: 'bar',
+  userId: 1,
+};
+axios.post(apiUrl, postData2)
+  .then(response => {
+    console.log('POST Request Response:', response.data);
+  })
+  .catch(error => {
+    console.error('POST Request Error:', error);
+  });
